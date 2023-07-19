38bf9e99f958e348194f95b2b308a1c2803b1a26
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2023-07-19 13:13:34 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -10,20 +10,26 @@ using namespace std;
 
 class Solution{
   public:
-    int f(int i, int j, string &s, vector<vector<int>>& dp) {
-        if(i == j) return 1;
-        if(i > j) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(s[i] == s[j]) return dp[i][j] = 2 + f(i+1, j-1, s,dp);
-        
-        return dp[i][j] = max(f(i+1, j, s,dp), f(i, j-1, s,dp));
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+
+        for(int j = 1 ; j<= m; j++) {
+            for(int i = 1; i<=n; i++) {
+                if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }         
+        }
+        return dp[n][m] == -1 ? 0 : dp[n][m];
     }
+  
     int longestPalinSubseq(string A) {
-        int n  = A.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return f(0, n-1, A, dp);
+        string s = A;
+        reverse(s.begin(),s.end());
+        return longestCommonSubsequence(s,A);
     }
 };
 
